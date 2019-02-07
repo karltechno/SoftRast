3340e3ea1677b3af14b21e0ccc742625e74d1ce5
Karl Davis <karltechnosound@gmail.com>
2019-02-07 22:11:16 +0000

    Optimise texture wrap calculation (replace lazy fmodf with manual frac calculation)
    Although there is a lot more to do before 'micro' optimisations, this literally showed up #2 on VTune and provides a noticeable frametime increase (~2-4ms)

--- SoftRast/Texture.cpp
+++ SoftRast/Texture.cpp
@@ -35,8 +35,8 @@ void TextureData::Clear()
 
 void SampleClamp_Slow(TextureData const& _tex, float const _u, float const _v, float o_colour[4])
 {
-	uint32_t const clampU = uint32_t(kt::Clamp<int32_t>(_u * _tex.m_width, 0, _tex.m_width - 1));
-	uint32_t const clampV = uint32_t(kt::Clamp<int32_t>(_v * _tex.m_height, 0, _tex.m_height - 1));
+	uint32_t const clampU = uint32_t(kt::Clamp<int32_t>(int32_t(_u * _tex.m_width), 0, _tex.m_width - 1));
+	uint32_t const clampV = uint32_t(kt::Clamp<int32_t>(int32_t(_v * _tex.m_height), 0, _tex.m_height - 1));
 
 	uint32_t const offs = clampV * _tex.m_rowStride + clampU * (_tex.m_bitsPerPixel / 8);
 	uint8_t* pix = _tex.m_data + offs;
@@ -49,14 +49,20 @@ void SampleClamp_Slow(TextureData const& _tex, float const _u, float const _v, f
 
 void SampleWrap_Slow(TextureData const& _tex, float const _u, float const _v, float o_colour[4])
 {
-	float uWrap = fmodf(_u, 1.0f);
-	float vWrap = fmodf(_v, 1.0f);
+	float const uSign = _u < 0.0f ? -1.0f : 1.0f;
+	float const vSign = _v < 0.0f ? -1.0f : 1.0f;
 
-	uWrap = uWrap < 0.0f ? 1.0f + uWrap : uWrap;
-	vWrap = vWrap < 0.0f ? 1.0f + vWrap : vWrap;
+	float const absU = uSign * _u;
+	float const absV = vSign * _v;
 
-	uint32_t const clampU = uint32_t(kt::Clamp<int32_t>(uWrap * _tex.m_width, 0, _tex.m_width - 1));
-	uint32_t const clampV = uint32_t(kt::Clamp<int32_t>(vWrap * _tex.m_height, 0, _tex.m_height - 1));
+	float const fracU = absU - int32_t(absU);
+	float const fracV = absV - int32_t(absV);
+
+	float const uWrap = uSign < 0.0f ? (1.0f - fracU) : fracU;
+	float const vWrap = vSign < 0.0f ? (1.0f - fracV) : fracV;
+
+	uint32_t const clampU = uint32_t(kt::Clamp<int32_t>(int32_t(uWrap * _tex.m_width), 0, _tex.m_width - 1));
+	uint32_t const clampV = uint32_t(kt::Clamp<int32_t>(int32_t(vWrap * _tex.m_height), 0, _tex.m_height - 1));
 
 	uint32_t const offs = clampV * _tex.m_rowStride + clampU * (_tex.m_bitsPerPixel / 8);
 	uint8_t* pix = _tex.m_data + offs;
